6cdeb4c9a8abbb64d102e03d3795b70645824a68
Warren Green <warren@trustedhousesitters.com>
2018-11-26 22:26:22 +0000

    updated open graphs

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,14 +23,16 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'Happy Pops is an indoor play area in the heart of Shoreham. It has been created to engage, entertain and educate kids up to the age of 5.' },
+            { name: 'keywords', content: 'shoreham, softplay, children, toddlers, play' },
+            { name: 'og:title', content: 'Happy Pops' },
+            { name: 'og:description', content: 'Happy Pops is an indoor play area in the heart of Shoreham. It has been created to engage, entertain and educate kids up to the age of 5.'},
+            { name: 'og:image', content: 'https://www.happypops.co.uk/static/footer-logo-300x131-f2d32ff1f96a343d8c911fceb124a64b-c0fdf.png' }
           ]}
         >
             <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"></link>
           <html lang="en" />
         </Helmet>
-        {/* <Navbar /> */}
         <Header />
 
         {children}
